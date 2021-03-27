b64619b869616fe9f033591223daf9cd04e01db0
madmandave <madmandave96@gmail.com>
2021-03-27 15:59:28 +0500

    newdashboard commit

--- src/components/pages/UserArea/udashboard.js
+++ src/components/pages/UserArea/udashboard.js
@@ -27,86 +27,110 @@ import {Modal,
   MenuCommand,
   MenuDivider,
   Textarea,
-  Icon} from '@chakra-ui/react'
+  AlertDialog,
+  AlertDialogBody,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogContent,
+  AlertDialogOverlay,
+  Center,
+  Icon,} from '@chakra-ui/react'
 import {ImMenu,ImExit} from 'react-icons/im'
 import {RiDashboardFill, RiSettings5Fill} from 'react-icons/ri'
 import {FaUserAlt,FaMapMarkedAlt,FaArrowUp,FaArrowDown,FaComment} from 'react-icons/fa'
-import {GiPistolGun} from 'react-icons/gi'
+import {GiPistolGun, GiTribalMask} from 'react-icons/gi'
 import {HiDotsHorizontal} from 'react-icons/hi'
 import {useState} from 'react'
 import Drawer from './useComponents/drawer'
+import Dashhome from './useComponents/Dashhome'
 import Post from './useComponents/Postdisplay'
+import LogoutAlert from './useComponents/LogoutAlert'
+import AddPostForum from './useComponents/AddPostforum'
+import { Add } from '@material-ui/icons'
 
 
 const Udashborad = () =>{
 const [MenuHidden,setHiddenMenu] = useState(true)
-// const [Post,setPost] = useState([
-//   {
-//     Title:"Robbery",
-//     Discription:"A pair of robbers took my possession at gun point",
-//     Location:"abclocation2ndstreetxyzcityof123state"
-//     }])
-const { isOpen, onOpen, onClose } = useDisclosure()
+// const [Title,setTitle] = useState('')
+// const [Discription,setDiscription] = useState('')
+// const [Location,setLocation] = useState('')
+const [Posts,setPost] = useState([
+    {
+    id:"1",
+    Title:"Robbery",
+    Discription:"A pair of robbers took my possession at gun point",
+    Location:"abclocation2ndstreetxyzcityof123state"
+    },
+    {
+    id:"2",
+    Title:"Robbery",
+    Discription:"A pair of robbers took my possession at gun point",
+    Location:"kanjortownexpress"
+    },
+    {
+    id:"3",
+    Title:"Robbery",
+    Discription:"A pair of robbers took my possession at gun point",
+    Location:"abclocation2ndstreetxyzcityof123state"
+    },
+    {
+      id:"4",
+      Title:"Robbery",
+      Discription:"A pair of robbers took my possession at gun point",
+      Location:"kanjortownexpress"
+    }
+      ])
+
+const Addpost = useDisclosure()
+const Logoffalert = useDisclosure()
+
+// Menu button to display task bar
 const toHide = () =>{
   setHiddenMenu((previous)=>{
    return !previous 
   })
 }
+// add post fuction 
+const makePost = (task) =>{
+  const id = Math.floor(Math.random()*1000)+1
+  const newPost = {id,...task}
+  setPost((previous)=>{return[newPost,...previous]})
+  
+}
+// remove a Post
+const removePost = () =>{}
+
+// logut function
+const loggingout = () =>{
+
+}
 
 return(
   <>
     <Head  Task={toHide}/>
-    <Flex>
-    <Drawer MenuHidden={MenuHidden}/>
-    <VStack position="static" w="100%" h="auto" bg="#8c8c8b" minH="calc(100vh - 78px)" overflow="hidden">
+    <Flex w="100%" height="100%">
+    <Drawer MenuHidden={MenuHidden} LastOpt={Logoffalert.onOpen}/>
+    <Box w="100%" h="calc(100vh - 78px)" bg="#8c8c8b"  overflowY="scroll">
     
-    <Box w="80%" h="auto" mt="10px" padding="15px" bg="#c1c1c1" borderRadius="5px"> 
-        
-        <Text mb="10px">Would you like to add a Crime! The gun icon is not for threats!</Text>
-        <Button float="right" onClick={onOpen} rightIcon={<Box as={GiPistolGun} fontSize="30px" />} >Add Crime</Button>
-        
-    </Box> 
+    
+    
+    <Dashhome Posts={Posts} Task={Addpost.onOpen}/>
+    
 
     
-    <Post/>
 
-    </VStack>
+    </Box>
     </Flex>
-    
-    <Modal isOpen={isOpen} onClose={onClose}>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>Add Crime</ModalHeader>
-          <ModalCloseButton />
-          
-          <ModalBody>
-          <form>
-            <Flex justify="space-between" mb="8px">
-                    <Box w="40%">
-                        <Text>Enter Longitude</Text>
-                        <Input w="100%"/>
-                    </Box>
-                    <Box w="40%">
-                        <Text>Enter Latitude</Text>
-                        <Input w="100%" />
-                    </Box>
-            </Flex>
-                <Box w="100%">
-                    <Text>Enter description</Text>
-                    <Textarea />
-                </Box>
-                <Button variant="solid" colorScheme="orange" w="100%">Add Images</Button>
-          </form>
-          </ModalBody>
 
-          <ModalFooter>
-            <Button colorScheme="yellow" mr={3} onClick={onClose}>
-              Close
-            </Button>
-            <Button variant="outline">Post</Button>
-          </ModalFooter>
-        </ModalContent>
-      </Modal>
+
+    {/*    Post forum     */}
+    <AddPostForum IsOverlayOpen={Addpost.isOpen} OverlayOnClose={Addpost.onClose} OnAdd={makePost}/>
+    
+
+    {/*   Logout Alert   */}
+    <LogoutAlert LogOverLayisOpen={Logoffalert.isOpen} LogOverLayonClose={Logoffalert.onClose}/>
+      
+
   </>
 )
 }
